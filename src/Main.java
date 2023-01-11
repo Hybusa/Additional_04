a32df2de30fee6145bf44e36b84be55036e6e872
KuZzz <alkouzne007@gmail.com>
2023-01-12 01:18:05 +0300

    Block completion

--- src/Main.java
+++ src/Main.java
@@ -78,22 +78,153 @@ public class Main {
             numsB3_sum += numsB3[i];
         }
 
-        if ((numsA3_sum / n3) > (numsB3_sum / n3))
-        {
+        if ((numsA3_sum / n3) > (numsB3_sum / n3)) {
             System.out.println("Массив А больше массива B");
-        }
-        else if ((numsB3_sum / n3) > (numsA3_sum / n3))
-        {
+        } else if ((numsB3_sum / n3) > (numsA3_sum / n3)) {
             System.out.println("Массив B больше массива A");
-        }
-        else
-        {
+        } else {
             System.out.println("Массивы равны");
         }
 
+        System.out.println(System.lineSeparator());
+
+        //Fourth task
+        /*
+            Создайте массив из 8 случайных целых чисел из отрезка [1;10].
+            Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль.
+            Снова выведете массив на экран на отдельной строке.
+        */
+
+        System.out.println("Задание 4. \n");
+
+        int[] nums4 = createRandom(4, 10, 99);
+
+        System.out.println(Arrays.toString(nums4));
+
+        boolean trigger = true;
+
+        for (int i = 0; i < nums4.length - 1; i++) {
+            if (nums4[i] > nums4[i + 1]) {
+                trigger = false;
+                break;
+            }
+
+        }
+
+        if (trigger)
+            System.out.println("Массив возрастающий");
+        else
+            System.out.println("Массив не возрастающий");
+
+        System.out.println(System.lineSeparator());
+
+
+        //Fifth task
+        /*
+           Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран.
+           Напоминаем, что первый и второй члены последовательности равны 0 и 1,
+           а каждый следующий — сумме двух предыдущих.
+        */
+
+        System.out.println("Задание 5. \n");
+
+        int [] fibArray = new int[20];
+        fibArray[0] = 0;
+        int fibN = 1;
+
+        for (int i = 1; i < fibArray.length; i++)
+        {
+            fibArray[i] = fibN;
+            fibN += fibArray[i-1];
+        }
+
+        System.out.println(Arrays.toString(fibArray));
+        System.out.println(System.lineSeparator());
+
+
+        //Sixth task
+        /*
+          Определите закономерность данных ниже последовательностей чисел
+          1, 4, 9, 16, 25...
+          1, -4, 9, -16, 25...
+          1, -1, 1, -1, 1, -1...
+          1, 0, 2, 0, 3, 0, 4...
+          Напишите код, который продолжит эти последовательности чисел до 10 элементов.
+        */
+
+        System.out.println("Задание 6. \n");
+
+        int [] firstArr = new int [10];
+
+        for (int i=0; i < firstArr.length;i++)
+        {
+            firstArr[i] = (i+1)*(i+1);
+        }
+
+        System.out.println("Первая плсдедовательность: \n" + Arrays.toString(firstArr));
+        System.out.println(System.lineSeparator());
+
+        int [] secondArr = new int [10];
+
+        boolean switcher1 = true;
+
+        for (int i=0; i < secondArr.length;i++)
+        {
+            secondArr[i] = (i+1)*(i+1);
+
+            if (!switcher1)
+            {
+                secondArr[i] *= -1;
+            }
+            switcher1 ^= true;
+        }
+
+        System.out.println("Вторая плсдедовательность: \n" + Arrays.toString(secondArr));
+        System.out.println(System.lineSeparator());
+
+
+        int [] thirdArr = new int [10];
+
+        boolean switcher2 = true;
+
+        for (int i=0; i < thirdArr.length;i++)
+        {
+            thirdArr[i] = 1;
+
+            if (!switcher2)
+            {
+                thirdArr[i] *= -1;
+            }
+            switcher2 ^= true;
+        }
+
+        System.out.println("Третья плсдедовательность: \n" + Arrays.toString(thirdArr));
+        System.out.println(System.lineSeparator());
+
+        int []  forthArr = new int [10];
+
+        int j = 1;
+
+        for (int i=0; i < forthArr.length;i++)
+        {
+            forthArr[i] = j;
+            i++;
+            j++;
+        }
+
+        System.out.println("Четвертая плсдедовательность: \n" + Arrays.toString(forthArr));
+        System.out.println(System.lineSeparator());
+
+
+
+
+
+
+
+
+
 
     }
-
 
     public static int[] createRandom(int n, int min, int max) {
         Random rd = new Random();
@@ -105,5 +236,4 @@ public class Main {
         }
         return array;
     }
-
 }
