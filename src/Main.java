2a13d8cea3cf3fdac3eeb13d2f830d5d906f6a0e
Hybusa <alkouzne007@gmail.com>
2023-01-13 20:21:42 +0300

    Block completion (major struct changes)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
+
+    public static void task1() {
+
         //First Task
         /*
         Создайте массив из 15 случайных целых чисел из отрезка [0;99].
@@ -12,19 +22,21 @@ public class Main {
         */
         System.out.println("Задание 1. \n");
 
-        int[] nums1 = createRandom(15, 0, 99);
-        int a1 = 0;
-        for (int j : nums1) {
+        int[] nums = createRandom(15, 0, 99);
+        int a = 0;
+        for (int j : nums) {
             if (j % 2 == 0) {
-                a1++;
+                a++;
             }
         }
 
-        System.out.println("Массив: " + Arrays.toString(nums1));
-        System.out.println("Количство четных элементов массива: " + a1);
+        System.out.println("Массив: " + Arrays.toString(nums));
+        System.out.println("Количство четных элементов массива: " + a);
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task2() {
         //Second task
         /*
             Создайте массив из 8 случайных целых чисел из отрезка [1;10].
@@ -34,59 +46,66 @@ public class Main {
 
         System.out.println("Задание 2. \n");
 
-        int[] nums2 = createRandom(8, 1, 10);
+        int[] nums = createRandom(8, 1, 10);
 
-        System.out.println("Массив: " + Arrays.toString(nums2));
+        System.out.println("Массив: " + Arrays.toString(nums));
 
 
-        for (int i = 0; i < nums2.length; i++) {
-            if (nums2[i] % 2 != 0) {
-                nums2[i] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i] = 0;
             }
         }
 
 
-        System.out.println("Замененный массив " + Arrays.toString(nums2));
+        System.out.println("Замененный массив " + Arrays.toString(nums));
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task3() {
         //Third task
         /*
-            Создайте массив из 8 случайных целых чисел из отрезка [1;10].
-            Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль.
-            Снова выведете массив на экран на отдельной строке.
+            Создайте 2 массива из 5 случайных целых чисел из отрезка [0;5] каждый,
+            выведите массивы в консоль в двух отдельных строках.
+            Посчитайте среднее арифметическое элементов каждого массива и сообщите,
+            для какого из массивов это значение оказалось больше
+            (либо сообщите, что их средние арифметические равны).
         */
 
         System.out.println("Задание 3. \n");
 
-        int n3 = 5;
+        int n = 5;
 
-        int[] numsA3 = createRandom(n3, 0, 5);
-        System.out.println(Arrays.toString(numsA3));
-        int[] numsB3 = createRandom(n3, 0, 5);
-        System.out.println(Arrays.toString(numsB3));
+        int[] numsA = createRandom(n, 0, 5);
+        System.out.println(Arrays.toString(numsA));
+        int[] numsB = createRandom(n, 0, 5);
+        System.out.println(Arrays.toString(numsB));
 
-        int numsA3_sum = 0;
+        int numsA_sum = 0;
 
-        for (int j : numsA3) {
-            numsA3_sum += j;
+        for (int j : numsA) {
+            numsA_sum += j;
         }
 
-        int numsB3_sum = 0;
+        int numsB_sum = 0;
 
-        for (int i = 0; i < numsA3.length; i++) {
-            numsB3_sum += numsB3[i];
+        for (int i = 0; i < numsA.length; i++) {
+            numsB_sum += numsB[i];
         }
 
-        if ((numsA3_sum / n3) > (numsB3_sum / n3)) {
+        if ((numsA_sum / n) > (numsB_sum / n)) {
             System.out.println("Массив А больше массива B");
-        } else if ((numsB3_sum / n3) > (numsA3_sum / n3)) {
+        } else if ((numsB_sum / n) > (numsA_sum / n)) {
             System.out.println("Массив B больше массива A");
         } else {
             System.out.println("Массивы равны");
         }
 
         System.out.println(System.lineSeparator());
+
+    }
+    public static void task4() {
 
         //Fourth task
         /*
@@ -97,14 +116,14 @@ public class Main {
 
         System.out.println("Задание 4. \n");
 
-        int[] nums4 = createRandom(4, 10, 99);
+        int[] nums = createRandom(4, 10, 99);
 
-        System.out.println(Arrays.toString(nums4));
+        System.out.println(Arrays.toString(nums));
 
         boolean trigger = true;
 
-        for (int i = 0; i < nums4.length - 1; i++) {
-            if (nums4[i] > nums4[i + 1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 trigger = false;
                 break;
             }
@@ -118,7 +137,9 @@ public class Main {
 
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task5() {
         //Fifth task
         /*
            Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран.
@@ -128,20 +149,21 @@ public class Main {
 
         System.out.println("Задание 5. \n");
 
-        int [] fibArray = new int[20];
+        int[] fibArray = new int[20];
         fibArray[0] = 0;
         int fibN = 1;
 
-        for (int i = 1; i < fibArray.length; i++)
-        {
+        for (int i = 1; i < fibArray.length; i++) {
             fibArray[i] = fibN;
-            fibN += fibArray[i-1];
+            fibN += fibArray[i - 1];
         }
 
         System.out.println(Arrays.toString(fibArray));
         System.out.println(System.lineSeparator());
 
+    }
 
+    public static void task6() {
         //Sixth task
         /*
           Определите закономерность данных ниже последовательностей чисел
@@ -154,26 +176,23 @@ public class Main {
 
         System.out.println("Задание 6. \n");
 
-        int [] firstArr = new int [10];
+        int[] firstArr = new int[10];
 
-        for (int i=0; i < firstArr.length;i++)
-        {
-            firstArr[i] = (i+1)*(i+1);
+        for (int i = 0; i < firstArr.length; i++) {
+            firstArr[i] = (i + 1) * (i + 1);
         }
 
         System.out.println("Первая плсдедовательность: \n" + Arrays.toString(firstArr));
         System.out.println(System.lineSeparator());
 
-        int [] secondArr = new int [10];
+        int[] secondArr = new int[10];
 
         boolean switcher1 = true;
 
-        for (int i=0; i < secondArr.length;i++)
-        {
-            secondArr[i] = (i+1)*(i+1);
+        for (int i = 0; i < secondArr.length; i++) {
+            secondArr[i] = (i + 1) * (i + 1);
 
-            if (!switcher1)
-            {
+            if (!switcher1) {
                 secondArr[i] *= -1;
             }
             switcher1 ^= true;
@@ -183,16 +202,14 @@ public class Main {
         System.out.println(System.lineSeparator());
 
 
-        int [] thirdArr = new int [10];
+        int[] thirdArr = new int[10];
 
         boolean switcher2 = true;
 
-        for (int i=0; i < thirdArr.length;i++)
-        {
+        for (int i = 0; i < thirdArr.length; i++) {
             thirdArr[i] = 1;
 
-            if (!switcher2)
-            {
+            if (!switcher2) {
                 thirdArr[i] *= -1;
             }
             switcher2 ^= true;
@@ -201,12 +218,11 @@ public class Main {
         System.out.println("Третья плсдедовательность: \n" + Arrays.toString(thirdArr));
         System.out.println(System.lineSeparator());
 
-        int []  forthArr = new int [10];
+        int[] forthArr = new int[10];
 
         int j = 1;
 
-        for (int i=0; i < forthArr.length;i++)
-        {
+        for (int i = 0; i < forthArr.length; i++) {
             forthArr[i] = j;
             i++;
             j++;
@@ -214,14 +230,6 @@ public class Main {
 
         System.out.println("Четвертая плсдедовательность: \n" + Arrays.toString(forthArr));
         System.out.println(System.lineSeparator());
-
-
-
-
-
-
-
-
 
 
     }
